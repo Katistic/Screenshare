fb8f57aed15073c825c6a8213fccc9b2b4a6e9a7
Katistic <33645731+Katistic@users.noreply.github.com>
2017-12-09 17:25:42 +1100

    Fixes

--- Screenshare/host/ScreenHost.py
+++ Screenshare/host/ScreenHost.py
@@ -4,36 +4,48 @@ import screen
 import os
 
 sockets = []
+output = []
+
+host = socket.gethostname()
+port = 4231
 
 def Loop():
-    screen.Get()
-    File = open("screenshot.png", "rb")
-    Data = File.read()
-    File.close()
-    os.remove("screenshot.png")
-    
-    for x in sockets():
-        x.send(Data)
+    while 1:
+        screen.Get()
+        File = open("screenshot.png", "rb")
+        Data = File.read()
+        File.close()
+        os.remove("screenshot.png")
+        
+        for x in sockets:
+            x.send(Data)
 
 def cls():
     print("\n"*150)
 
-print("Starting Send Loop...")
-thread = threading.Thread(target = Loop())
+def pprint(t):
+    cls()
+    output.append(t)
+
+    for x in output:
+        print(x)
+
+    print("Host: "+host)
+    print("Port: "+str(port))
+
+pprint("Starting Send Loop...")
+thread = threading.Thread(target = Loop)
 thread.start()
 
-print("Creating server...")
+pprint("Creating server...")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 4231
 s.bind((host, port))
 cls()
-print("Server created.")
-print("Host: "+host)
-print("Port: "+port)
+pprint("Server created.")
 s.listen(5)
 
 while 1:
     cs, addr = s.accept()
     sockets.append(cs)
+    pprint("New connection: "+str(addr))
--- Screenshare/host/screen.py
+++ Screenshare/host/screen.py
@@ -5,8 +5,11 @@ screen = wx.ScreenDC()
 size = screen.GetSize()
 
 def Get():
-    bmp = wx.EmptyBitmap(size[0], size[1])
-    mem = wx.MemoryDC(bmp)
+    bmp = wx.Bitmap(size[0], size[1])
+    mem = wx.MemoryDC()
+    mem.SelectObject(bmp)
     mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
-    del mem
+    mem.SelectObject(wx.NullBitmap)
+    img = bmp.ConvertToImage()
     bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
+    del mem
