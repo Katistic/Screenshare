c690695e51019208b23a26e3c9a097a45432e2f6
Katistic <33645731+Katistic@users.noreply.github.com>
2017-12-13 11:58:57 +1100

    Create screen TEST

---
+++ Tests/Screen.py
@@ -0,0 +1,10 @@
+def Get():
+    bmp = wx.Bitmap(size[0], size[1])
+    mem = wx.MemoryDC()
+    mem.SelectObject(bmp)
+    mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
+    mem.SelectObject(wx.NullBitmap)
+    img = bmp.ConvertToImage()
+    #bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
+    del mem
+    return bmp
