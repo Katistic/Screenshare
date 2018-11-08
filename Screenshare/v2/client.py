610b97a1c438ff1985be9a6f8117d3bd097a2618
Katistic <33645731+Katistic@users.noreply.github.com>
2018-11-09 10:00:55 +1100

    Base
    I don't expect this to work tbh

--- Screenshare/v2/client.py
+++ Screenshare/v2/client.py
@@ -1,1 +1,20 @@
+import socket, pickle, sys, time
 
+if 'pyscreenshot' in sys.modules:
+    import pyscreenshot as ImageGrab
+else:
+    print("Please install pyscreenshot!\npip install pyscreenshot")
+    while True:
+        time.sleep(60)
+    exit()
+
+HOST = input("Host: ")
+PORT = 61734
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+while True:
+    im_string = str(sock.recv(1024*1024), "utf-8")
+    im = pickle.loads(im_string)
+    
+    im.show()
